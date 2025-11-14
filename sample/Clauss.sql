9fbf11454026b34850772c04f06bcd2d38d49e84
AB_GOWDA <abhishekgowda8090@gmail.com>
2025-11-14 06:59:40 +0530

    Completed with the Clauses in SQL

---
+++ sample/Clauss.sql
@@ -0,0 +1,25 @@
+USE MyDatabase;
+GO
+
+SELECT *
+FROM customers
+ORDER BY country ASC,
+		score DESC;
+
+
+SELECT 
+	country,
+	COUNT(ID) AS [TOTAL_NO_CUSTOMER],
+	SUM(score) AS [TOTAL_SCORES]
+FROM customers
+GROUP BY country
+
+GO
+
+SELECT 
+	country,
+	SUM(score) AS [Total Score]
+FROM customers
+WHERE score != 0
+GROUP BY country
+HAVING SUM(score) > 430;
